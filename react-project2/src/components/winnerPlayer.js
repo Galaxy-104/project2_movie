1ab489182dc19f94736f452d314bde40302ee1d2
Galaxy <js_park95@naver.com>
2023-10-16 14:21:16 +0900

    add 토너먼트 페이지 애니메이션 구현 완료

--- react-project2/src/components/winnerPlayer.js
+++ react-project2/src/components/winnerPlayer.js
@@ -8,24 +8,21 @@ function WinnerPlayer({ player, handleClick, direction, isVisible, className }){
     console.log(player)
     return (
         <AnimatePresence>
-            {isVisible && (
-                <motion.div
-
-                    className={`player ${direction === 0 ? 'left' : 'right'} ${className === undefined? "" : className}`} 
-                    onClick={handleClick}
-                    key={player.id}
-
-                    // initial={{ opacity: 0, y: 20 }}
-                    // animate={{ opacity: 1, y: 0 }}
-                    // exit={{ opacity: 0, y: 20 }}
-                >
-                    <div className="player-img">
-                        <Champion/>
-                        <img src={`https://image.tmdb.org/t/p/original/${player.poster_path}`} alt={`${player.title}`}/>                                           
-                    </div>
-                    
-                </motion.div>            
-            )}
+            
+            <motion.div
+                className={`player ${direction === 0 ? 'left' : 'right'} ${className === undefined? "" : className}`} 
+                onClick={handleClick}
+                key={player.id}
+                // initial={{ opacity: 0, y: 20 }}
+                // animate={{ opacity: 1, y: 0 }}
+                // exit={{ opacity: 0, y: 20 }}
+            >
+                <div className="player-img">
+                    <Champion/>
+                    <img src={`https://image.tmdb.org/t/p/original/${player.poster_path}`} alt={`${player.title}`}/>                                           
+                </div>
+                
+            </motion.div>            
              
         </AnimatePresence>
         
--- react-project2/src/pages/Tournament.js
+++ react-project2/src/pages/Tournament.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from "react";
-import { useAsyncError, useNavigate } from "react-router-dom";
+import { useAsyncError, useInRouterContext, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion"
 
 import Match from "../components/Match";
@@ -52,87 +52,178 @@ function Tournament(){
 
     const [ direction, setDirection ] = useState("")
     const [ isSelected, setIsSelected ] = useState(false)
-    const [ round, setRound ] = useState(16)
+
     const [ quarter, setQuarter ] = useState([])
+    const [ isQuarter, setIsQuarter ] = useState(false)
+    const [ semi, setSemi ] = useState([])
+    const [ isSemi, setIsSemi ] = useState(false)
+    const [ final, setFinal ] = useState([])
+    const [ isFinal, setIsFinal ] = useState(false)
 
     const selectLeft = () => {
         console.log("왼쪽 선택")
         setDirection("left")
         setIsSelected(true)
-        setRound(round - 1)        
+        setCounter(counter - 1)
     }
 
     const selectRight = () => {
         console.log("오른쪽 선택")
         setDirection("right")
         setIsSelected(true)
-        setRound(round - 1)
+        setCounter(counter - 1)
     }
 
     useEffect(() => {
 
         if(isSelected){
             setMatchIndex(matchIndex + 2)
+            if(!isQuarter){
+                if(direction === "left"){
+                    setQuarter([...quarter, movies[matchIndex]])
+                }else if(direction === "right"){
+                    setQuarter([...quarter, movies[matchIndex + 1]])
+                }
+            }else if(!isSemi){
+                if(direction === "left"){
+                   setSemi([...semi, movies[matchIndex]])
+                }else if(direction === "right"){
+                   setSemi([...semi, movies[matchIndex + 1]])
+                }
+            }else if(!isFinal){
+                if(direction === "left"){
+                    setFinal([...final, movies[matchIndex]])
+                 }else if(direction === "right"){
+                    setFinal([...final, movies[matchIndex + 1]])
+                 }
+            }else{
+                if(direction === "left"){
+                    setWinner([movies[matchIndex]])
+                 }else if(direction === "right"){
+                    setWinner([movies[matchIndex + 1]])
+                 }
+            }
         }
-
+            
     }, [isSelected])
 
     useEffect(() => {
         setIsSelected(false)
     }, [matchIndex])
 
+    useEffect(() => {
+        if(counter === 8){
+            setIsQuarter(true)
+            setMatchIndex(0)
+        }else if(counter === 4){
+            setIsSemi(true)
+            setMatchIndex(0)
+        }else if(counter === 2){
+            setIsFinal(true)
+            setMatchIndex(0)
+        }
+    }, [counter])
+
+    useEffect(() => {
+        if(isQuarter){
+            setMovies([...quarter])
+        }else if(isSemi){
+            setMovies([...semi])
+        }else if(isFinal){
+            setMovies([...final])
+        }
+    }, [isQuarter, isSemi, isFinal])
+
+    const navigate = useNavigate()
+    const moveToResult = () => navigate('/result', { state: {winner, result: winnerMoviesGenre}})
+
+    useEffect(() => {
+        if(isFinal && winner.length === 1){
+            const filterMovies = tournamentMovies.filter((movie) => {
+                const findMv = movie.movies.find((mv) => {
+                     if(mv.id === winner[0].id){
+                         return mv
+                     }
+                 })
+                 return findMv !== undefined
+            })
+            // console.log(filterMovies)
+            fetch('http://127.0.0.1:5201/api/result'
+            ,{
+                method: 'PUT',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    id: filterMovies[0].code
+                })
+            })
+            setWinnerMoivesGenre([...filterMovies])
+        }
+
+    }, [winner])
+
+    useEffect(() => {
+        if(isFinal){
+           setTimeout(moveToResult, 1000) 
+        }
+    }, [winnerMoviesGenre])
+
     if(loading){
 
         return (
             <div className="tournament-page">
-                {console.log("log: 렌더링 완료", movies)}
-                <TimeBar counter={counter}/>
-                <motion.div 
-                    className="match-container"
-                    key="match-container"
-                >
-                    {movies.length === 0? 
-                    <LoadingPage/>
-                     :
-                    <AnimatePresence exitBeforeEnter>
-                        <motion.div
-                            className="match-left"
-                            whileHover={isSelected? "" : { scale: 1.1 }}
-                            initial={isSelected? "" :{ y: 10, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            exit={isSelected? 
-                                direction === "left"?
-                                { x: 400, opacity: 1, scale: 1.1 } : { y: 50, opacity: 0, scale: 0.9 } : 
-                                ""
-                            }
-                            transition={isSelected && direction === "left"? { duration: 1 } : { duration: 0.4 }}
+                {winner.length === 1?
+                    <WinnerPlayer player={winner[0]}/> :
+                <>  
+                    {console.log(counter, matchIndex)}
+                    <TimeBar counter={counter}/>
+                    <motion.div 
+                        className="match-container"
+                        key="match-container"
+                    >
+                        {movies.length === 0? 
+                        <LoadingPage/>
+                        :
+                        <AnimatePresence exitBeforeEnter>
+                            {console.log(match)}
+                            <motion.div
+                                className="match-left"
+                                whileHover={isSelected? "" : { scale: 1.1 }}
+                                initial={isSelected? "" :{ y: 10, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                exit={isSelected? 
+                                    direction === "left"?
+                                    { x: 400, opacity: 1, scale: 1.1 } : { y: 50, opacity: 0, scale: 0.9 } : 
+                                    ""
+                                }
+                                transition={isSelected && direction === "left"? { duration: 1 } : { duration: 0.4 }}
 
-                            key={`tounament-${movies[matchIndex].id}`}
-                        >
-                            <Player player={movies[matchIndex]} handleClick={selectLeft} direction={0} isVisible={isVisible}/>
-                        </motion.div>
+                                key={`tounament-${movies[matchIndex].id}`}
+                            >
+                                <Player player={movies[matchIndex]} handleClick={selectLeft} direction={0} isVisible={isVisible}/>
+                            </motion.div>
+                        
+                            <motion.div 
+                                className="match-right"
+                                whileHover={isSelected? "" : { scale: 1.1}}
+                                initial={isSelected? "" : { y: 10, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                exit={isSelected?
+                                    direction === "right"?
+                                    { x: -400, opacity: 1, scale: 1.1 } : { y: 50, opacity: 0, scale: 0.9 } :
+                                    ""
+                                }
+                                transition={isSelected && direction === "right"? { duration: 1 } : { duration: 0.4 }}
+                                
+                                key={`tounament-${movies[matchIndex + 1].id}`}
+                            >
+                                <Player player={movies[matchIndex + 1]} handleClick={selectRight} direction={1} isVisible={isVisible}/>
+                            </motion.div>
+                        </AnimatePresence>
+                        }
                     
-                        <motion.div 
-                            className="match-right"
-                            whileHover={isSelected? "" : { scale: 1.1}}
-                            initial={isSelected? "" : { y: 10, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            exit={isSelected?
-                                direction === "right"?
-                                { x: -400, opacity: 1, scale: 1.1 } : { y: 50, opacity: 0, scale: 0.9 } :
-                                ""
-                            }
-                            transition={isSelected && direction === "right"? { duration: 1 } : { duration: 0.4 }}
-                            
-                            key={`tounament-${movies[matchIndex + 1].id}`}
-                        >
-                            <Player player={movies[matchIndex + 1]} handleClick={selectRight} direction={1} isVisible={isVisible}/>
-                        </motion.div>
-                    </AnimatePresence>
-                    }
-                   
-                </motion.div>
-
+                    </motion.div>
+                </>
+            }   
             </div>
             
         )
